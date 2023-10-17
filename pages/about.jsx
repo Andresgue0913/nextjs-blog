72a3ffaf30b0945681bb115339b777f2a46fecb8
Andresgue0913 <141692743+Andresgue0913@users.noreply.github.com>
2023-10-17 14:51:57 -0500

    primer node

---
+++ pages/about.jsx
@@ -0,0 +1,12 @@
+import Layout from "@/components/Layout"
+
+export default function about() {
+  return (
+    <Layout
+    title="about | next.js"
+    description="This is a about"
+    >
+      <h1>About</h1>
+    </Layout>
+  )
+}
---
+++ pages/contact.jsx
@@ -0,0 +1,12 @@
+import Layout from "@/components/Layout"
+
+export default function contact() {
+  return (
+    <Layout
+    title="Contact | next.js"
+    description="This is a Contact"
+    >
+      <h1>contact</h1>
+    </Layout>
+  )
+}
