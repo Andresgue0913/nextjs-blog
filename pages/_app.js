8cda9830a1120945bb925ba2c264f554baea64f6
amingomezd <amingomezd@gmail.com>
2023-10-25 16:51:04 -0500

    adding material ui library and config

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,40 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import 'config/globalStyles.css';
+import { Toaster } from 'react-hot-toast';
 
-function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+/**
+ * MUI Imports and Configs
+ */
+import PropTypes from 'prop-types';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider } from '@emotion/react';
+import createEmotionCache from 'config/createEmotionCache';
+import { useMediaQuery } from '@mui/material';
+import React, { useMemo } from 'react';
+import { theme } from 'config/theme';
+
+const clientSideEmotionCache = createEmotionCache();
+
+export default function MyApp({ Component, pageProps, emotionCache = clientSideEmotionCache }) {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const customTheme = useMemo(() => theme(prefersDarkMode), [prefersDarkMode]);
+
+  return (
+      <CacheProvider value={emotionCache}>
+        <ThemeProvider theme={customTheme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+          <Toaster />
+        </ThemeProvider>
+      </CacheProvider>
+  );
 }
 
-export default App;
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  emotionCache: PropTypes.object,
+  pageProps: PropTypes.object.isRequired
+};
+
